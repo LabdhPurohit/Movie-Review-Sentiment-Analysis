f7b66cfb7f29249dae26881de614218d2ad57155
LabdhPurohit <80207914+LabdhPurohit@users.noreply.github.com>
2025-03-20 17:45:00 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
     
     e.preventDefault();
 
-    const response = await fetch("https://fastapireviewsentiment-production.up.railway.app/analyze/", {
+    const response = await fetch("http://127.0.0.1:8000/analyze/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ review }),
